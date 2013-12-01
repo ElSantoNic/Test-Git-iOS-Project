439ddc0ee2a9fe9e9a959e221aa7d1bf6a07da1b
ElSantoNic <nick.santana@gmail.com>
2013-11-30 23:38:14 -0500

    added a test class

---
+++ Git Test/SNTest.h
@@ -0,0 +1,13 @@
+//
+//  SNTest.h
+//  Git Test
+//
+//  Created by Nicolas Santana on 11/30/13.
+//  Copyright (c) 2013 Tortilla White. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SNTest : NSObject
+
+@end
---
+++ Git Test/SNTest.m
@@ -0,0 +1,13 @@
+//
+//  SNTest.m
+//  Git Test
+//
+//  Created by Nicolas Santana on 11/30/13.
+//  Copyright (c) 2013 Tortilla White. All rights reserved.
+//
+
+#import "SNTest.h"
+
+@implementation SNTest
+
+@end
